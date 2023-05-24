3d037fcc6083c3aef8d4ba83b26981bd049e080b
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:07 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/neovimport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/neovimport.git'), string(name: 'PORT_DESCRIPTION', value: 'Neovim is a Vim-based text editor engineered for extensibility and usability, to encourage new applications and contributions.[D[C[D[C' )]
   }
