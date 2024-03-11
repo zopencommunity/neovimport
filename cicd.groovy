bfed2883b65094c1f5a6a4b6338982666f8cd11f
Igor Todorovski <itodorov@ca.ibm.com>
2024-03-11 17:22:06 -0400

    first commit

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
                         userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/neovimport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/neovimport.git'), string(name: 'PORT_DESCRIPTION', value: 'Neovim is a Vim-based text editor engineered for extensibility and usability, to encourage new applications and contributions.[D[C[D[C' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/neovimport.git'), string(name: 'PORT_DESCRIPTION', value: 'Neovim is a Vim-based text editor engineered for extensibility and usability, to encourage new applications and contributions' )]
   }
 }
