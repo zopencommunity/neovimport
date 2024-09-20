9809748017d74d79135752b87535f8c50f5219c2
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:33 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/neovimport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/neovimport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/neovimport.git'), string(name: 'PORT_DESCRIPTION', value: 'Neovim is a Vim-based text editor engineered for extensibility and usability, to encourage new applications and contributions' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/neovimport.git'), string(name: 'PORT_DESCRIPTION', value: 'Neovim is a Vim-based text editor engineered for extensibility and usability, to encourage new applications and contributions' )]
   }
 }
